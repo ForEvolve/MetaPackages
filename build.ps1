739a5eea87bf5064539759ae01e1bf9ce554d34f
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-07-29 17:16:59 -0400

    Removed OutputDirectory and disabled tests

--- build.ps1
+++ build.ps1
@@ -5,12 +5,12 @@ param(
     [Alias('c')]
     [string]$Configuration = 'Debug',
     [string]$MetaPackagePackageReferenceVersion = '1.0.0-upsilon-*',
-    [Alias('o')]
-    [string]$OutputDirectory = '../../nuget-packages'
+    #    [Alias('o')]
+    #    [string]$OutputDirectory = '../../nuget-packages'
 ) 
 
 dotnet clean
 dotnet restore --configfile $ConfigFile /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
-dotnet build src/ForEvolve/ForEvolve.csproj --no-restore -c $Configuration /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion --output $OutputDirectory
-dotnet test test/ForEvolve.Tests/ForEvolve.Tests.csproj /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
+dotnet build src/ForEvolve/ForEvolve.csproj --no-restore -c $Configuration /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion# --output $OutputDirectory
+# dotnet test test/ForEvolve.Tests/ForEvolve.Tests.csproj /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
 # dotnet pack --no-build /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion -c $Configuration
