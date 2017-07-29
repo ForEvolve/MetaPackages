be63f5f2a9ae3f0001d6b20f4d7cb6c26c7045fc
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-07-29 17:31:19 -0400

    Added some Write-Output to help trace errors

--- build.ps1
+++ build.ps1
@@ -4,13 +4,23 @@ param(
     [string]$ConfigFile = '../../NuGet.config',
     [Alias('c')]
     [string]$Configuration = 'Debug',
-    [string]$MetaPackagePackageReferenceVersion = '1.0.0-upsilon-*'#,
-    #    [Alias('o')]
-    #    [string]$OutputDirectory = '../../nuget-packages'
+    [string]$MetaPackagePackageReferenceVersion = '1.0.0-upsilon-*'
 ) 
 
+Write-Output 'dotnet clean starting'
 dotnet clean
+Write-Output 'dotnet clean completed'
+
+Write-Output 'dotnet restore starting'
 dotnet restore --configfile $ConfigFile /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
-dotnet build src/ForEvolve/ForEvolve.csproj --no-restore -c $Configuration /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion # --output $OutputDirectory
+Write-Output 'dotnet restore completed'
+
+Write-Output 'dotnet build starting'
+dotnet build src/ForEvolve/ForEvolve.csproj --no-restore -c $Configuration /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
+Write-Output 'dotnet build completed'
+
+# Write-Output 'dotnet test starting'
 # dotnet test test/ForEvolve.Tests/ForEvolve.Tests.csproj /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
+# Write-Output 'dotnet test completed'
+
 # dotnet pack --no-build /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion -c $Configuration
