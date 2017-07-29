4bda94084136f4c1e4a77ad68656b0e9b25ba203
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-07-29 15:20:30 -0400

    Included DynamicInternalServerError in build
    Updated the build script
    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # MetaPackages
 ForEvolve Framework metapackages
 
-
 **This is still in prototyping mode**
+
+[![forevolve MyGet Build Status](https://www.myget.org/BuildSource/Badge/forevolve?identifier=b15d1c32-b86e-4a55-a497-229071f23e70)](https://www.myget.org/)
--- build.ps1
+++ build.ps1
@@ -1,2 +1,12 @@
-dotnet restore
-dotnet pack
+param(
+    [Alias('v')]
+    [string]$PackageVersion,
+    [string]$ConfigFile = '../../NuGet.config',
+    [Alias('c')]
+    [string]$Configuration = 'Debug'
+)
+
+dotnet clean
+dotnet restore --configfile $ConfigFile /p:PackageVersion=$PackageVersion
+dotnet build --no-restore -c $Configuration /p:PackageVersion=$PackageVersion
+dotnet pack --no-build --no-restore /p:PackageVersion=$PackageVersion -c $Configuration
