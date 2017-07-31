00b9e16fc3c479337695dc25d3011c5a3df887c4
Carl-Hugo Marcotte <Carl-Hugo@users.noreply.github.com>
2017-07-31 00:28:29 -0400

    CI/CD pipeline (#1)
    Updated the build script
    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # MetaPackages
-ForEvolve Framework metapackages
+This the home of the ForEvolve Framework meta-packages.
 
+To be simpler, I created this repository containing a NuGet package that includes all other ForEvolve packages.
+*I may create more meta-packages in the future, hence the plural name of the repository.*
+
+It should be easier to use the ForEvolve Framework without knowing the packages division.
+If you prefer fine-grained packages referencing, you can still do it.
 
 **This is still in prototyping mode**
+
+- VSTS: ![ForEvolve VSTS Build Status](https://forevolve.visualstudio.com/_apis/public/build/definitions/b800edd0-96da-46c1-a089-06a4466e62d9/17/badge)
+- MyGet: [NuGet V3 feed URL](https://www.myget.org/F/forevolve/api/v3/index.json)
--- build.ps1
+++ build.ps1
@@ -1,2 +1,28 @@
-dotnet restore
-dotnet pack
+param(
+    [Alias('v')]
+    [string]$PackageVersion,
+    [string]$ConfigFile = '../../NuGet.config',
+    [Alias('c')]
+    [string]$Configuration = 'Debug',
+    [string]$MetaPackagePackageReferenceVersion = '1.0.0-upsilon-*'
+) 
+
+$TraceColor = 'DarkCyan'
+
+Write-Host 'dotnet clean starting' -foregroundcolor $TraceColor
+dotnet clean
+Write-Host 'dotnet clean completed' -foregroundcolor $TraceColor
+
+Write-Host 'dotnet restore starting' -foregroundcolor $TraceColor
+dotnet restore --configfile $ConfigFile /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
+Write-Host 'dotnet restore completed' -foregroundcolor $TraceColor
+
+Write-Host 'dotnet build starting' -foregroundcolor $TraceColor
+dotnet build src/ForEvolve/ForEvolve.csproj -c $Configuration /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
+Write-Host 'dotnet build completed' -foregroundcolor $TraceColor
+
+Write-Host 'dotnet test starting' -foregroundcolor $TraceColor
+dotnet test test/ForEvolve.Tests/ForEvolve.Tests.csproj /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion
+Write-Host 'dotnet test completed' -foregroundcolor $TraceColor
+
+# dotnet pack --no-build /p:PackageVersion=$PackageVersion /p:MetaPackagePackageReferenceVersion=$MetaPackagePackageReferenceVersion -c $Configuration
